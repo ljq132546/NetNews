f8daaa2934b0da88f6e5f589b72d32f6b0c0ae62
jiqing-j-liu <jiqing.j.liu@cnsdc01.pwc.com>
2017-05-10 14:54:14 +0800

    no message

---
+++ customerImage.swift
@@ -0,0 +1,13 @@
+//
+//  customerImage.swift
+//  ChartsTest
+//
+//  Created by Jiqing J Liu on 5/10/17.
+//  Copyright © 2017 jiqing. All rights reserved.
+//
+
+import UIKit
+
+class customerImage: UIImage {
+
+}
